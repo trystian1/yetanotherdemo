0e0075273d8d71104c332097b1067ceff0d8e03e
tofferman <tofferman@bol.com>
2023-01-29 12:01:33 +0100

    demo-time

---
+++ src/main/java/com/example/yetanotherdemo/YetanotherdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.yetanotherdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetanotherdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetanotherdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yetanotherdemo/YetanotherdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yetanotherdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetanotherdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
